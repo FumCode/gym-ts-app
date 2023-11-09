1e4e7b220159ed58f59588a17a3a1d902328221c
FumCode <fumcode@gmail.com>
2023-11-09 21:49:55 +0200

    add actionbutton styles

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -1,7 +1,21 @@
-type Props = {};
-const ActionButton = (props: Props) => {
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import React from "react";
+import {SelectedPage} from "@/shared/types.ts";
+
+type Props = {
+    children: React.ReactNode;
+    setSelectedPage: (value: SelectedPage) => void;
+};
+const ActionButton = ({ children, setSelectedPage }: Props) => {
     return (
-        <div>ActionButton</div>
+        <AnchorLink
+            className='rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500
+            hover:text-white'
+            onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+            href={`#${SelectedPage.ContactUs}`}
+        >
+            {children}
+        </AnchorLink>
     );
 };
 export default ActionButton;
