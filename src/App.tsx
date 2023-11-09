bb3bf306bcd85d9e13e521c2ac2db0b0128d58e0
FumCode <fumcode@gmail.com>
2023-11-09 15:59:41 +0200

    add imports and alias

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,12 @@
 
 function App() {
   return (
-    <>
+    <div className='app'>
         <h1 className="text-3xl font-bold underline mb-20">
             Hello world!
         </h1>
 
-    </>
+    </div>
   )
 }
 
