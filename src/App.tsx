0bdce98358b60439c26bc77c47593a59480bcf66
FumCode <fumcode@gmail.com>
2023-11-09 16:19:32 +0200

    add prettier-plugin

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@
 function App() {
   return (
     <div className='app'>
-        <h1 className="text-3xl font-bold underline mb-20">
+        <h1 className="text-3xl font-bold underline mb-20 ml-20">
             Hello world!
         </h1>
 
