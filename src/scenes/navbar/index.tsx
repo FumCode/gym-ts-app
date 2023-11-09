4da473092512778b1dd5c4cc9644bcbf744cf916
FumCode <fumcode@gmail.com>
2023-11-09 21:18:52 +0200

    add mobile menu button

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,7 @@ import { Bars3Icon, XMarkIcon} from '@heroicons/react/24/solid';
 import Logo from '@/assets/Logo.png';
 import Link from "@/scenes/navbar/Link.tsx";
 import {SelectedPage} from "@/shared/types.ts";
+import useMediaQuery from "@/hooks/useMediaQuery.ts";
 
 
 type Props = {
@@ -14,6 +15,9 @@ const Navbar = ({
     setSelectedPage,
                 }: Props) => {
     const flexBetween = 'flex items-center justify-between';
+    const isAboveMediumScreens = useMediaQuery('(min-width: 1060px)')
+    const [isMenuToggled, setMenuToggled] = useState<boolean>(false)
+
     return (
         <nav>
             <div
@@ -25,34 +29,42 @@ const Navbar = ({
                         <img src={Logo} alt="logo"/>
 
                     {/*Right Side*/}
-                        <div className={`${flexBetween} w-full `}>
+                        {isAboveMediumScreens ? (
+                            <div className={`${flexBetween} w-full `}>
                             <div className={`${flexBetween} gap-8 text-sm`}>
                                 <Link
                                     page='Home'
                                     selectedPage={selectedPage}
-                                    setSelectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
                                 />
                                 <Link
                                     page='Benefits'
                                     selectedPage={selectedPage}
-                                    setSelectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
                                 />
                                 <Link
                                     page='Our Classes'
                                     selectedPage={selectedPage}
-                                    setSelectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
                                 />
                                 <Link
                                     page='Contact Us '
                                     selectedPage={selectedPage}
-                                    setSelectedPage={selectedPage}
+                                    setSelectedPage={setSelectedPage}
                                 />
                             </div>
                             <div className={`${flexBetween} gap-8`}>
                                 <p>Sign In</p>
                                 <button>Become a Member</button>
                             </div>
-                        </div>
+                        </div>) : (
+                            <button
+                            className='rounded-full bg-secondary-500 p-2'
+                            onClick={() => setIsMenuToggled(!isMenuToggled)}
+                            >
+                                <Bars3Icon className='h-6 w-6 text-white' />
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
