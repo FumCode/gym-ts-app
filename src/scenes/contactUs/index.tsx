c750c721f392a9691afd2952cc175f4b6b7806b3
FumCode <fumcode@gmail.com>
2023-11-12 21:10:18 +0200

    create contactUs file

---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,11 @@
+import {SelectedPage} from "@/shared/types.ts";
+
+type Props = {
+setSelectedPage: (value: SelectedPage) => void;
+};
+const ContactUs = ({ setSelectedPage }: Props) => {
+    return (
+        <div>contactUs</div>
+    );
+};
+export default ContactUs;
